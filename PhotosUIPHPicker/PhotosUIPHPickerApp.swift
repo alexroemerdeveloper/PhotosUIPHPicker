0da017661fe51950fb707bb54e6eb891fb5210ef
Alexander Römer <alex@roemer.design>
2020-09-26 19:21:29 +0200

    Initial Commit

---
+++ PhotosUIPHPicker/PhotosUIPHPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosUIPHPickerApp.swift
+//  PhotosUIPHPicker
+//
+//  Created by Alexander Römer on 26.09.20.
+//
+
+import SwiftUI
+
+@main
+struct PhotosUIPHPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
